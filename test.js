13d299c83eb018f0108ddea8248d22bb48baa0ee
Grosze <mateuszk827@wp.pl>
2021-03-09 13:07:15 +0100

    travis POST test

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const supertest = require("supertest");
 const server = supertest.agent("http://localhost:8080");
 
 describe("GET test", () => {
-    it("should return hello", (done) => {
+    it("should return hello", done => {
         server
             .get("/")
             .expect("Content-type", /text/)
@@ -11,5 +11,20 @@ describe("GET test", () => {
             .end((err, res) => {
                 done();
             });
+    });
+});
+
+describe("POST test", () => {
+    it("should return 0", done => {
+        server
+            .post('/')
+            .send({input: "john"})
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then(response => {
+                assert(response.res, 0)
+            })
+            .catch(err => done(err))
     });
 });
